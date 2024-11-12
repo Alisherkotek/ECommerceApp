e2790351dbd4d8bace0bb027961bc30425af585b
alisherkotek <konysbek8@gmail.com>
2024-11-12 15:18:44 +0500

    payment

---
+++ Infrastructure/Migrations/20241111074354_DeliveryMethodsAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeliveryMethodsAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DeliveryItem",
+                table: "DeliveryMethods",
+                newName: "DeliveryTime");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DeliveryTime",
+                table: "DeliveryMethods",
+                newName: "DeliveryItem");
+        }
+    }
+}
