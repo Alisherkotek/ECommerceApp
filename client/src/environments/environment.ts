0706dc1fdfdb9c4bb47658458da9da0770e47457
alisherkotek <konysbek8@gmail.com>
2024-11-17 16:47:26 +0500

    Pre CI

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'api/',
     hubUrl: 'hub/notifications',
-    stripePublicKey: 'pk_test_51QJsPoAgpEZdnWB7PtD87qCjOTKjmBAKvi8LpcSJSIc9fAw6WcVLIyKKsLHGZ2FAHNYrYDD8tXzwkaTERPH3YD4F00VLQriFF1'
+    stripePublicKey: 'pk_test_51QM3b4LD5rzQu9peXODNr6NtfC4H1sCM45YffS4kq3z4ZB8xTgge5yr9Au2Z1tJ5e80fr0XYvr30lSPZHsWHxq5V00Zm7DXBs4'
 };
