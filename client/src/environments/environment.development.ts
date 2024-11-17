fd98f94693b20f5f8ab95870af4730262b1a0d4f
alisherkotek <konysbek8@gmail.com>
2024-11-17 17:54:20 +0500

    to deploy check

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     apiUrl: 'https://localhost:5001/api/',
     hubUrl: 'https://localhost:5001/hub/notifications',
-    stripePublicKey: 'pk_test_51QJsPoAgpEZdnWB7PtD87qCjOTKjmBAKvi8LpcSJSIc9fAw6WcVLIyKKsLHGZ2FAHNYrYDD8tXzwkaTERPH3YD4F00VLQriFF1'
+    stripePublicKey: 'pk_test_51QM3b4LD5rzQu9peXODNr6NtfC4H1sCM45YffS4kq3z4ZB8xTgge5yr9Au2Z1tJ5e80fr0XYvr30lSPZHsWHxq5V00Zm7DXBs4'
 };
 
 
